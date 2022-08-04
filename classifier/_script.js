675e1b1e15a7075001dbf64b9115dcc34faef948
Colin Brooks <mcbrooks@pm.me>
2022-08-04 19:25:35 -0400

    created webcam based classifier, renamed prior MNIST classifier files

---
+++ classifier/_script.js
@@ -0,0 +1,97 @@
+import { MnistData } from "./data.js";
+
+function getModel() {
+    const model = tf.sequential();
+    const IMAGE_WIDTH = 28;
+    const IMAGE_HEIGHT = 28;
+    const IMAGE_CHANNELS = 1;
+    
+    model.add(tf.layers.conv2d({inputShape: [IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS],kernelSize: 5,filters: 8,strides: 1,activation: 'relu',kernelInitializer: 'varianceScaling'}));
+
+    // The MaxPooling layer acts as a sort of downsampling using max values in a region instead of averaging.  
+    model.add(tf.layers.maxPooling2d({poolSize: [2, 2], strides: [2, 2]}));
+
+    // Repeat another conv2d + maxPooling stack. 
+    model.add(tf.layers.conv2d({kernelSize: 5,filters: 16,strides: 1,activation: 'relu',kernelInitializer: 'varianceScaling'}));
+    model.add(tf.layers.maxPooling2d({poolSize: [2, 2], strides: [2, 2]}));
+
+    // Now we flatten the output from the 2D filters into a 1D vector to prepare it for input into our last layer. This is common practice when feedinghigher dimensional data to a final classification output layer.
+    model.add(tf.layers.flatten());
+
+    // Our last layer is a dense layer which has 10 output units, one for each output class (i.e. 0, 1, 2, 3, 4, 5, 6, 7, 8, 9).
+    const NUM_OUTPUT_CLASSES = 10;
+    model.add(tf.layers.dense({units: NUM_OUTPUT_CLASSES,kernelInitializer: 'varianceScaling',activation: 'softmax'}));
+
+    // Choose an optimizer, loss function and accuracy metric,
+    // then compile and return the model
+    const optimizer = tf.train.adam();
+    model.compile({
+        optimizer: optimizer,
+        loss: 'categoricalCrossentropy',
+        metrics: ['accuracy'],
+    });
+
+    return model;
+}
+
+async function train(model, data) {
+    const metrics = ['loss', 'val_loss', 'acc', 'val_acc'];
+    const container = {
+      name: 'Model Training', tab: 'Model', styles: { height: '1000px' }
+    };
+    const fitCallbacks = tfvis.show.fitCallbacks(container, metrics);
+    const BATCH_SIZE = 512;
+    const TRAIN_DATA_SIZE = 55000;
+    const TEST_DATA_SIZE = 10000;
+    const [trainXs, trainYs] = tf.tidy(() => {
+      const d = data.nextTrainBatch(TRAIN_DATA_SIZE);
+      return [
+        d.xs.reshape([TRAIN_DATA_SIZE, 28, 28, 1]),d.labels];
+    });
+
+   const [testXs, testYs] = tf.tidy(() => {
+      const d = data.nextTestBatch(TEST_DATA_SIZE);
+      return [
+        d.xs.reshape([TEST_DATA_SIZE, 28, 28, 1]),d.labels];
+    });
+
+   return model.fit(trainXs, trainYs, {
+     batchSize: BATCH_SIZE,
+     validationData: [testXs, testYs],
+     epochs: 10,
+     shuffle: true,
+     callbacks: fitCallbacks
+   });
+}
+
+const classNames = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+function doPrediction(model, data, testDataSize = 500) { 
+  const IMAGE_WIDTH = 28;
+  const IMAGE_HEIGHT = 28;
+  const testData = data.nextTestBatch(testDataSize);
+  const testxs = testData.xs.reshape([testDataSize, IMAGE_WIDTH, IMAGE_HEIGHT, 1]);
+  const labels = testData.labels.argMax(-1);
+  const preds = model.predict(testxs).argMax(-1);
+  testxs.dispose();
+  return [preds, labels];
+}
+
+async function run() {  
+    const data = new MnistData();
+    await data.load();
+    const model = getModel();
+    tfvis.show.modelSummary({name: 'Model Architecture', tab: 'Model'}, model);
+    await train(model, data);
+  }
+  document.addEventListener('DOMContentLoaded', run);
+
+async function showAccuracy(model, data) {
+    const [preds, labels] = doPrediction(model, data);
+    preds.print()
+    const classAccuracy = await tfvis.metrics.perClassAccuracy(labels, preds);
+    const container = {name: 'Accuracy', tab: 'Evaluation'};
+    tfvis.show.perClassAccuracy(container, classAccuracy, classNames);
+    labels.dispose();
+}
+
+await showAccuracy(model, data);
--- classifier/script.js
+++ classifier/script.js
@@ -1,97 +1,211 @@
-import { MnistData } from "./data.js";
-
-function getModel() {
-    const model = tf.sequential();
-    const IMAGE_WIDTH = 28;
-    const IMAGE_HEIGHT = 28;
-    const IMAGE_CHANNELS = 1;
-    
-    model.add(tf.layers.conv2d({inputShape: [IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS],kernelSize: 5,filters: 8,strides: 1,activation: 'relu',kernelInitializer: 'varianceScaling'}));
-
-    // The MaxPooling layer acts as a sort of downsampling using max values in a region instead of averaging.  
-    model.add(tf.layers.maxPooling2d({poolSize: [2, 2], strides: [2, 2]}));
-
-    // Repeat another conv2d + maxPooling stack. 
-    model.add(tf.layers.conv2d({kernelSize: 5,filters: 16,strides: 1,activation: 'relu',kernelInitializer: 'varianceScaling'}));
-    model.add(tf.layers.maxPooling2d({poolSize: [2, 2], strides: [2, 2]}));
-
-    // Now we flatten the output from the 2D filters into a 1D vector to prepare it for input into our last layer. This is common practice when feedinghigher dimensional data to a final classification output layer.
-    model.add(tf.layers.flatten());
-
-    // Our last layer is a dense layer which has 10 output units, one for each output class (i.e. 0, 1, 2, 3, 4, 5, 6, 7, 8, 9).
-    const NUM_OUTPUT_CLASSES = 10;
-    model.add(tf.layers.dense({units: NUM_OUTPUT_CLASSES,kernelInitializer: 'varianceScaling',activation: 'softmax'}));
-
-    // Choose an optimizer, loss function and accuracy metric,
-    // then compile and return the model
-    const optimizer = tf.train.adam();
-    model.compile({
-        optimizer: optimizer,
-        loss: 'categoricalCrossentropy',
-        metrics: ['accuracy'],
-    });
-
-    return model;
+const STATUS = document.getElementById('status');
+if (STATUS) {
+  STATUS.innerText = 'Loaded TensorFlow.js - version: ' + tf.version.tfjs;
 }
 
-async function train(model, data) {
-    const metrics = ['loss', 'val_loss', 'acc', 'val_acc'];
-    const container = {
-      name: 'Model Training', tab: 'Model', styles: { height: '1000px' }
+const VIDEO = document.getElementById('webcam');
+const ENABLE_CAM_BUTTON = document.getElementById('enableCam');
+const RESET_BUTTON = document.getElementById('reset');
+const TRAIN_BUTTON = document.getElementById('train');
+const MOBILE_NET_INPUT_WIDTH = 224;
+const MOBILE_NET_INPUT_HEIGHT = 224;
+const STOP_DATA_GATHER = -1;
+const CLASS_NAMES = [];
+
+ENABLE_CAM_BUTTON.addEventListener('click', enableCam);
+TRAIN_BUTTON.addEventListener('click', trainAndPredict);
+RESET_BUTTON.addEventListener('click', reset);
+
+
+let dataCollectorButtons = document.querySelectorAll('button.dataCollector');
+for (let i = 0; i < dataCollectorButtons.length; i++) {
+  dataCollectorButtons[i].addEventListener('mousedown', gatherDataForClass);
+  dataCollectorButtons[i].addEventListener('mouseup', gatherDataForClass);
+  // Populate the human readable names for classes.
+  CLASS_NAMES.push(dataCollectorButtons[i].getAttribute('data-name'));
+}
+
+
+let mobilenet = undefined;
+let gatherDataState = STOP_DATA_GATHER;
+let videoPlaying = false;
+let trainingDataInputs = [];
+let trainingDataOutputs = [];
+let examplesCount = [];
+let predict = false;
+
+
+/**
+ * Loads the MobileNet model and warms it up so ready for use.
+ **/
+ async function loadMobileNetFeatureModel() {
+  const URL = 
+    'https://tfhub.dev/google/tfjs-model/imagenet/mobilenet_v3_small_100_224/feature_vector/5/default/1';
+  
+  mobilenet = await tf.loadGraphModel(URL, {fromTFHub: true});
+  STATUS.innerText = 'MobileNet v3 loaded successfully!';
+  
+  // Warm up the model by passing zeros through it once.
+  tf.tidy(function () {
+    let answer = mobilenet.predict(tf.zeros([1, MOBILE_NET_INPUT_HEIGHT, MOBILE_NET_INPUT_WIDTH, 3]));
+    console.log(answer.shape);
+  });
+}
+
+// Call the function immediately to start loading.
+loadMobileNetFeatureModel();
+
+
+
+let model = tf.sequential();
+model.add(tf.layers.dense({inputShape: [1024], units: 128, activation: 'relu'}));
+model.add(tf.layers.dense({units: CLASS_NAMES.length, activation: 'softmax'}));
+
+model.summary();
+
+// Compile the model with the defined optimizer and specify a loss function to use.
+model.compile({
+  // Adam changes the learning rate over time which is useful.
+  optimizer: 'adam',
+  // Use the correct loss function. If 2 classes of data, must use binaryCrossentropy.
+  // Else categoricalCrossentropy is used if more than 2 classes.
+  loss: (CLASS_NAMES.length === 2) ? 'binaryCrossentropy': 'categoricalCrossentropy', 
+  // As this is a classification problem you can record accuracy in the logs too!
+  metrics: ['accuracy']  
+});
+
+
+
+
+function hasGetUserMedia() {
+  return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+}
+
+function enableCam() {
+  if (hasGetUserMedia()) {
+    // getUsermedia parameters.
+    const constraints = {
+      video: true,
+      width: 640, 
+      height: 480 
     };
-    const fitCallbacks = tfvis.show.fitCallbacks(container, metrics);
-    const BATCH_SIZE = 512;
-    const TRAIN_DATA_SIZE = 55000;
-    const TEST_DATA_SIZE = 10000;
-    const [trainXs, trainYs] = tf.tidy(() => {
-      const d = data.nextTrainBatch(TRAIN_DATA_SIZE);
-      return [
-        d.xs.reshape([TRAIN_DATA_SIZE, 28, 28, 1]),d.labels];
+
+    // Activate the webcam stream.
+    navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+      VIDEO.srcObject = stream;
+      VIDEO.addEventListener('loadeddata', function() {
+        videoPlaying = true;
+        ENABLE_CAM_BUTTON.classList.add('removed');
+      });
     });
-
-   const [testXs, testYs] = tf.tidy(() => {
-      const d = data.nextTestBatch(TEST_DATA_SIZE);
-      return [
-        d.xs.reshape([TEST_DATA_SIZE, 28, 28, 1]),d.labels];
-    });
-
-   return model.fit(trainXs, trainYs, {
-     batchSize: BATCH_SIZE,
-     validationData: [testXs, testYs],
-     epochs: 10,
-     shuffle: true,
-     callbacks: fitCallbacks
-   });
-}
-
-const classNames = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
-function doPrediction(model, data, testDataSize = 500) { 
-  const IMAGE_WIDTH = 28;
-  const IMAGE_HEIGHT = 28;
-  const testData = data.nextTestBatch(testDataSize);
-  const testxs = testData.xs.reshape([testDataSize, IMAGE_WIDTH, IMAGE_HEIGHT, 1]);
-  const labels = testData.labels.argMax(-1);
-  const preds = model.predict(testxs).argMax(-1);
-  testxs.dispose();
-  return [preds, labels];
-}
-
-async function run() {  
-    const data = new MnistData();
-    await data.load();
-    const model = getModel();
-    tfvis.show.modelSummary({name: 'Model Architecture', tab: 'Model'}, model);
-    await train(model, data);
+  } else {
+    console.warn('getUserMedia() is not supported by your browser');
   }
-  document.addEventListener('DOMContentLoaded', run);
-
-async function showAccuracy(model, data) {
-    const [preds, labels] = doPrediction(model, data);
-    preds.print()
-    const classAccuracy = await tfvis.metrics.perClassAccuracy(labels, preds);
-    const container = {name: 'Accuracy', tab: 'Evaluation'};
-    tfvis.show.perClassAccuracy(container, classAccuracy, classNames);
-    labels.dispose();
 }
 
-await showAccuracy(model, data);
+
+
+/**
+ * Handle Data Gather for button mouseup/mousedown.
+ **/
+ function gatherDataForClass() {
+  let classNumber = parseInt(this.getAttribute('data-1hot'));
+  gatherDataState = (gatherDataState === STOP_DATA_GATHER) ? classNumber : STOP_DATA_GATHER;
+  dataGatherLoop();
+}
+
+
+
+
+function dataGatherLoop() {
+  if (videoPlaying && gatherDataState !== STOP_DATA_GATHER) {
+    let imageFeatures = tf.tidy(function() {
+      let videoFrameAsTensor = tf.browser.fromPixels(VIDEO);
+      let resizedTensorFrame = tf.image.resizeBilinear(videoFrameAsTensor, [MOBILE_NET_INPUT_HEIGHT, 
+          MOBILE_NET_INPUT_WIDTH], true);
+      let normalizedTensorFrame = resizedTensorFrame.div(255);
+      return mobilenet.predict(normalizedTensorFrame.expandDims()).squeeze();
+    });
+
+    trainingDataInputs.push(imageFeatures);
+    trainingDataOutputs.push(gatherDataState);
+    
+    // Intialize array index element if currently undefined.
+    if (examplesCount[gatherDataState] === undefined) {
+      examplesCount[gatherDataState] = 0;
+    }
+    examplesCount[gatherDataState]++;
+
+    STATUS.innerText = '';
+    for (let n = 0; n < CLASS_NAMES.length; n++) {
+      STATUS.innerText += CLASS_NAMES[n] + ' data count: ' + examplesCount[n] + '. ';
+    }
+    window.requestAnimationFrame(dataGatherLoop);
+  }
+}
+
+
+
+
+async function trainAndPredict() {
+  predict = false;
+  tf.util.shuffleCombo(trainingDataInputs, trainingDataOutputs);
+  let outputsAsTensor = tf.tensor1d(trainingDataOutputs, 'int32');
+  let oneHotOutputs = tf.oneHot(outputsAsTensor, CLASS_NAMES.length);
+  let inputsAsTensor = tf.stack(trainingDataInputs);
+  
+  let results = await model.fit(inputsAsTensor, oneHotOutputs, {shuffle: true, batchSize: 5, epochs: 10, 
+      callbacks: {onEpochEnd: logProgress} });
+  
+  outputsAsTensor.dispose();
+  oneHotOutputs.dispose();
+  inputsAsTensor.dispose();
+  predict = true;
+  predictLoop();
+}
+
+function logProgress(epoch, logs) {
+  console.log('Data for epoch ' + epoch, logs);
+}
+
+
+
+function predictLoop() {
+  if (predict) {
+    tf.tidy(function() {
+      let videoFrameAsTensor = tf.browser.fromPixels(VIDEO).div(255);
+      let resizedTensorFrame = tf.image.resizeBilinear(videoFrameAsTensor,[MOBILE_NET_INPUT_HEIGHT, 
+          MOBILE_NET_INPUT_WIDTH], true);
+
+      let imageFeatures = mobilenet.predict(resizedTensorFrame.expandDims());
+      let prediction = model.predict(imageFeatures).squeeze();
+      let highestIndex = prediction.argMax().arraySync();
+      let predictionArray = prediction.arraySync();
+
+      STATUS.innerText = 'Prediction: ' + CLASS_NAMES[highestIndex] + ' with ' + Math.floor(predictionArray[highestIndex] * 100) + '% confidence';
+    });
+
+    window.requestAnimationFrame(predictLoop);
+  }
+}
+
+
+
+
+/**
+ * Purge data and start over. Note this does not dispose of the loaded 
+ * MobileNet model and MLP head tensors as you will need to reuse 
+ * them to train a new model.
+ **/
+ function reset() {
+  predict = false;
+  examplesCount.length = 0;
+  for (let i = 0; i < trainingDataInputs.length; i++) {
+    trainingDataInputs[i].dispose();
+  }
+  trainingDataInputs.length = 0;
+  trainingDataOutputs.length = 0;
+  STATUS.innerText = 'No data collected';
+  
+  console.log('Tensors in memory: ' + tf.memory().numTensors);
+}
