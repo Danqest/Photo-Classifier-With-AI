5029f6e302f7e4658e86e93cc4caae0c672fe60c
Alfredo Figueroa <alfredojfigueroa@gmail.com>
2022-08-04 21:39:17 -0400

    removing node_modules

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,10 @@
 import React from "react";
-import ProductList from "../components/ProductList";
-import CategoryMenu from "../components/CategoryMenu";
-import Cart from "../components/Cart";
+import Sidebar from "../components/Sidebar";
 
 const Home = () => {
   return (
     <div className="container">
-      <CategoryMenu />
-      <ProductList />
-      <Cart />
+      <Sidebar />
     </div>
   );
 };
