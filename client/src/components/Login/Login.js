d49c3e3b7262052b6dde7507adc620afd69de2d7
Colin Brooks <mcbrooks@pm.me>
2022-08-12 19:10:27 -0400

    updated client files

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,22 +1,23 @@
-import React, { useState } from 'react';
-import { useMutation } from '@apollo/client';
-import { Link } from 'react-router-dom';
-import { LOGIN } from '../../utils/mutations';
-import Auth from '../../utils/auth';
-import './Login.css';
+import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { Link } from "react-router-dom";
+import { LOGIN } from "../../utils/mutations";
+import Auth from "../../utils/auth";
+import "./Login.css";
 
 function Login(props) {
-  const [formState, setFormState] = useState({ email: '', password: '' });
-  const [login, { error }] = useMutation(LOGIN);
+  const [formState, setFormState] = useState({ email: "", password: "" });
+  const [login, { error, data }] = useMutation(LOGIN);
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
+    console.log(formState);
     try {
-      const mutationResponse = await login({
-        variables: { email: formState.email, password: formState.password },
+      const { data } = await login({
+        variables: { ...formState },
       });
-      const token = mutationResponse.data.login.token;
-      Auth.login(token);
+
+      Auth.login(data.login.token);
     } catch (e) {
       console.log(e);
     }
@@ -31,52 +32,59 @@ function Login(props) {
   };
 
   return (
-    <form className='container' onSubmit={handleFormSubmit}>
-        <h3>Sign In</h3>
-        <div className="mb-3">
-          <label>Email address</label>
+    <form className="container" onSubmit={handleFormSubmit}>
+      <h3>Sign In</h3>
+      <div className="mb-3">
+        <label>Email address</label>
+        <input
+          type="email"
+          className="form-control"
+          placeholder="Enter email"
+          name="email"
+          id="email"
+          value={formState.email}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="mb-3">
+        <label>Password</label>
+        <input
+          type="password"
+          className="form-control"
+          placeholder="Enter password"
+          name="password"
+          id="password"
+          value={formState.password}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="mb-3">
+        <div className="custom-control custom-checkbox">
           <input
-            type="email"
-            className="form-control"
-            placeholder="Enter email"
-            id='email'
-            onChange={handleChange}
+            type="checkbox"
+            className="custom-control-input"
+            id="customCheck1"
           />
+          <label className="custom-control-label" htmlFor="customCheck1">
+            Remember me
+          </label>
         </div>
-        <div className="mb-3">
-          <label>Password</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Enter password"
-            id='pwd'
-            onChange={handleChange}
-          />
-        </div>
-        <div className="mb-3">
-          <div className="custom-control custom-checkbox">
-            <input
-              type="checkbox"
-              className="custom-control-input"
-              id="customCheck1"
-            />
-            <label className="custom-control-label" htmlFor="customCheck1">
-              Remember me
-            </label>
-          </div>
-        </div>
-        <div className="d-grid">
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
-        </div>
-        <p className="forgot-password text-right">
-          Forgot <a href="#">password?</a>
-        </p>
-        <p className='no-account text-center'>
-          Don't Have an Account? Click <span><Link to='/Signup'>here</Link></span>
-        </p>
-      </form>
+      </div>
+      <div className="d-grid">
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </div>
+      <p className="forgot-password text-right">
+        Forgot <a href="#">password?</a>
+      </p>
+      <p className="no-account text-center">
+        Don't Have an Account? Click{" "}
+        <span>
+          <Link to="/Signup">here</Link>
+        </span>
+      </p>
+    </form>
   );
 }
 
