c35a81f6964b3e5e2f9bad7a49970be444721ea9
Colin Brooks <mcbrooks@pm.me>
2022-08-11 21:21:12 -0400

    edited so server can run

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -17,25 +17,21 @@ const typeDefs = gql`
   type Subfolder {
     _id: ID
     subfolderName: String
-    images: [Image]
   }
 
   type Query {
     users: [User]
     collections: [Collection]
     subfolders: [Subfolder]
-    collections(id: ID!): Collection
   }
 
   type Mutation {
     addCollection(collectionTitle: String!): Collection
     addSubfolder(subfolderName: String!): Subfolder
-    # Define the required parameters for updating a class
-    updateClass(id: ID!, building: String!): Class
     # Define the required parameters for updating a collection
-    updateCollection(id: ID!, collectionTitle: String!): Class
+    updateCollection(id: ID!, collectionTitle: String!): Collection
     # Define the required parameters for updating a subfolder
-    updateSubfolder(id: ID!, subfolderName: String!): Class
+    updateSubfolder(id: ID!, subfolderName: String!): Subfolder
   }
 `;
 
