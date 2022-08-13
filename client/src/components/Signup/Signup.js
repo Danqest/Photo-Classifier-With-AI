4b821c8088aad9d4277fdeb5e002bc79563157f8
Colin Brooks <mcbrooks@pm.me>
2022-08-12 20:00:58 -0400

    added header and signup functionality

--- client/src/components/Signup/Signup.js
+++ client/src/components/Signup/Signup.js
@@ -37,50 +37,61 @@ function Signup() {
 
 
   return (
-    <form onSubmit={handleFormSubmit}>
-        <h3>Sign Up</h3>
-        <div className="mb-3">
-          <label>Username</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Enter Username"
-            id='firstName'
-            name='username'
-            onChange={handleChange}
-          />
-        </div>
-        <div className="mb-3">
-          <label>Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Enter Email"
-            id='email'
-            name='email'
-            onChange={handleChange}
-          />
-        </div>
-        <div className="mb-3">
-          <label>Password</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Enter Password"
-            id='pwd'
-            name='password'
-            onChange={handleChange}
-          />
-        </div>
-        <div className="d-grid">
-          <button type="submit" className="btn btn-primary">
-            Sign Up
-          </button>
-        </div>
-        <p className='no-account text-center'>
-          Already have an Account? Click <span><Link to='./login'>here</Link></span>
-        </p>
-      </form>
+      <div>
+      {data ? (
+              <p>
+                Success! You may now head{' '}
+                <Link to="/">back to the homepage.</Link>
+              </p>
+            ) : (
+    
+        <form onSubmit={handleFormSubmit}>
+            <h3>Sign Up</h3>
+            <div className="mb-3">
+              <label>Username</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter Username"
+                id='firstName'
+                name='username'
+                onChange={handleChange}
+              />
+            </div>
+            <div className="mb-3">
+              <label>Email address</label>
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Enter Email"
+                id='email'
+                name='email'
+                onChange={handleChange}
+              />
+            </div>
+            <div className="mb-3">
+              <label>Password</label>
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Enter Password"
+                id='pwd'
+                name='password'
+                onChange={handleChange}
+              />
+            </div>
+            <div className="d-grid">
+              <button type="submit" className="btn btn-primary">
+                Sign Up
+              </button>
+            </div>
+            <p className='no-account text-center'>
+              Already have an Account? Click <span><Link to='./login'>here</Link></span>
+            </p>
+          </form>
+        )
+      } 
+    </div>
   );
 }
 
